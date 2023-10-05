0887523a368fcff23b8456eec683278bda3fbdf7
Артем Стихин <mamba.kolt@mail.ru>
2023-10-05 07:56:24 +0500

    без кода

--- pr_1/Form1.Designer.cs
+++ pr_1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(252, 141);
+            this.ClientSize = new System.Drawing.Size(439, 294);
             this.Name = "Form1";
             this.Text = "Form";
             this.Load += new System.EventHandler(this.Form1_Load);
