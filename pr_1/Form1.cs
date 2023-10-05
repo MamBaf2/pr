d5e3df1262f578f86942c8489bf6d5643f707395
Артем Стихин <mamba.kolt@mail.ru>
2023-10-06 02:38:51 +0500

    3 кроме цветов

--- pr_1/Form1.cs
+++ pr_1/Form1.cs
@@ -21,5 +21,41 @@ namespace pr_1
         {
 
         }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            //Show the Open File dialog. If the user clicks OK, load the
+            //picture that the user chose.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //clear the picture
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
     }
 }
