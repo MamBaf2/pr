78b6f7f88eff30e5d61b190f4ef1776a6980c5ff
Артем Стихин <mamba.kolt@mail.ru>
2023-10-14 00:57:21 +0500

    Без комментов

--- pr_1/Form1.cs
+++ pr_1/Form1.cs
@@ -12,14 +12,90 @@ namespace pr_1
 {
     public partial class Form1 : Form
     {
+        Label firstClicked = null;
+        Label secondClicked = null;
+        Random random = new Random();
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
         }
-
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel1 = control as Label;
+                if (iconLabel1 != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel1.Text = icons[randomNumber];
+                    iconLabel1.ForeColor = iconLabel1.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+                CheckForWinner();
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+            firstClicked = null;
+            secondClicked = null;
+        }
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel1 = control as Label;
+                if (iconLabel1 != null)
+                {
+                    if (iconLabel1.ForeColor == iconLabel1.BackColor)
+                        return;
+                }
+            }
+
+
+
+            MessageBox.Show("Дмитрий, вы молодец! ","Поставьте 5!");
+            Close();
         }
     }
 }
