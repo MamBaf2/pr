91f5dd3faa6d8d8d324710dc4bc45ec6f2d99cd1
Артём Стихин <artem-stikhin@mail.ru>
2023-10-26 10:01:01 +0500

    Проверочная

--- pr_1/Form1.Designer.cs
+++ pr_1/Form1.Designer.cs
@@ -28,21 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pct = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pct
+            // 
+            this.pct.Location = new System.Drawing.Point(33, 50);
+            this.pct.Name = "pct";
+            this.pct.Size = new System.Drawing.Size(154, 138);
+            this.pct.TabIndex = 0;
+            this.pct.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(252, 141);
+            this.ClientSize = new System.Drawing.Size(487, 328);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pct);
             this.Name = "Form1";
             this.Text = "Form";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pct)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pct;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
