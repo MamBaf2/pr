d12470b84317e5426e516f86e243074ae056b3ed
Артем Стихин <mamba.kolt@mail.ru>
2023-10-31 01:44:16 +0500

    переделать немного код

--- pr_1/Form1.Designer.cs
+++ pr_1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace pr_1
 {
-    partial class Form1
+    partial class Lab7
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,21 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtPrecision = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(12, 265);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(99, 27);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Вычислить";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(61, 143);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(85, 20);
+            this.txtX.TabIndex = 1;
+            // 
+            // txtPrecision
+            // 
+            this.txtPrecision.Location = new System.Drawing.Point(263, 143);
+            this.txtPrecision.Name = "txtPrecision";
+            this.txtPrecision.Size = new System.Drawing.Size(85, 20);
+            this.txtPrecision.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(58, 215);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "label1";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(23, 71);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(199, 13);
+            this.lblQuantity.TabIndex = 4;
+            this.lblQuantity.Text = "The sequrence is x + x/2 + x/3 +x/4 + ...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "X =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "presicion =";
+            // 
+            // Lab7
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(252, 141);
-            this.Name = "Form1";
-            this.Text = "Form";
+            this.ClientSize = new System.Drawing.Size(360, 316);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtPrecision);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.btnCalc);
+            this.Name = "Lab7";
+            this.Text = "Сумма ряда";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtPrecision;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- pr_1/Form1.cs
+++ pr_1/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace pr_1
 {
-    public partial class Form1 : Form
+    public partial class Lab7 : Form
     {
-        public Form1()
+        public Lab7()
         {
             InitializeComponent();
         }
@@ -20,6 +20,46 @@ namespace pr_1
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        //кнопка "Вычислить".
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            // Объявляем две переменные x и precision для ввода пользовательских значений.
+            double x, precision;
+            // Проверяем, успешно ли удалось преобразовать текст из txtX и txtPrecision в числа типа double.
+            if (double.TryParse(txtX.Text, out x) && double.TryParse(txtPrecision.Text, out precision))
+            {
+                /* Объявляем несколько переменных для вычислений:
+                 sum - сумма, начальное значение 0.
+                 slog - текущее слагаемое, начальное значение равно x.
+                 addslog - счетчик количества слагаемых, начальное значение 1.
+                 f - флаг для отслеживания успешности вычислений.*/
+                double sum = 0;
+                double slog = x;
+                int addslog = 1;
+                bool f = false;
+                // Начинаем цикл, который будет выполняться, пока текущее слагаемое по модулю больше или равно заданной точности.
+                while (Math.Abs(slog) >= precision)
+                {
+                    // Добавляем текущее слагаемое к сумме.
+                    sum += slog;
+                    // Увеличиваем счетчик слагаемых.
+                    addslog++;
+                    // Вычисляем следующее слагаемое как x, деленное на addslog.
+                    slog = x / addslog;
+                }
+                // Выводим сумму в элемент управления lblResult.
+                lblResult.Text = "sum = " + sum.ToString();
+                // Выводим количество слагаемых в элемент управления lblQuantity.
+                lblQuantity.Text = "quantity = " + addslog.ToString();
+                // Устанавливаем флаг f в true, чтобы показать, что вычисления успешно завершились.
+                f = true;
+            }
+            else
+            {
+                // Если пользователь ввел некорректные значения (например, не числа), выводим сообщение об ошибке.
+                MessageBox.Show("Введите числовое значение");
+            }
         }
     }
 }
