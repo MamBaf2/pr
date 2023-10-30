413cc8bd4e12bc4ac79a81a0ba7bf6a07e2f7a4d
Артем Стихин <mamba.kolt@mail.ru>
2023-10-31 00:55:20 +0500

    allready

--- pr_1/Form1.cs
+++ pr_1/Form1.cs
@@ -16,39 +16,53 @@ namespace pr_1
         {
             InitializeComponent();
         }
-
+        //пустая форма никому не нужная
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
+        //Присваиваем переменной animation значение false
+        private bool animation = false;
+        //timer
         private void tmr_Tick(object sender, EventArgs e)
         {
+            // Увеличиваем позицию изображения на 20 пикселей вправо
             pct.Left = pct.Left + 20;
+            // Проверяем, достигло ли изображение правого края формы.
             if (pct.Left + pct.Width > this.Width)
             {
+                // Если достигло, останавливаем таймер.
                 tmr.Stop();
+                // Меняем текст кнопки на "Старт".
                 btnStart.Text = "Старт";
-                pct.Left = 0;
+                // Выходим из этого метода.
                 return;
             }
         }
-        private bool animation = false;
+        //кнопка старт/стоп
         private void btnStart_Click(object sender, EventArgs e)
         {
             if (animation)
             {
-                //возвращает изображение в начальное положение
-                //(устанавливает значение свойства Left изображения pct в 0 пикселей)
+                // Если анимация выполняется, останавливаем таймер и меняем текст кнопки на "Старт"
                 tmr.Stop();
                 btnStart.Text = "Старт";
+                // Устанавливаем значение анимации в false
+                animation = false;
+            }
+            // Если изображение достигло правого края, возвращаем его в начальную позицию
+            else if (pct.Left + pct.Width > this.Width)
+            {
+                pct.Left = 0;
+                // Устанавливаем значение анимации в false
                 animation = false;
             }
             else
             {
+                /* Если анимация не выполняется и изображение не на правом краю,
+                запускаем таймер, меняем текст кнопки на "Стоп" и устанавливаем значение анимации в true*/
                 tmr.Start();
                 btnStart.Text = "Стоп";
-                //Устанавливает значение переменной(выполняется)
                 animation = true;
                 tmr.Interval = 50;
             }
@@ -57,6 +71,7 @@ namespace pr_1
 
         private void btnExit_Click(object sender, EventArgs e)
         {
+            //Закрытие формы
             this.Close();
         }
     }
